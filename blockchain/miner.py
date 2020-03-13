2cca366794aba4a7bb36c7ac9de4600cabc8e43f
Forrest-Darabian <mr.smiley72@yahoo.com>
2020-03-13 09:46:02 -0700

    Need to work on questions, MVP met

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -9,6 +9,8 @@ from timeit import default_timer as timer
 
 import random
 
+from random import randint
+
 
 def proof_of_work(last_proof):
     """
@@ -23,8 +25,10 @@ def proof_of_work(last_proof):
     start = timer()
 
     print("Searching for next proof")
-    proof = 0
-    #  TODO: Your code here
+    last_hash = hashlib.sha256(f'{last_proof}'.encode()).hexdigest()
+    proof = last_proof*randint(1, 100)
+    while valid_proof(last_hash, proof) is False:
+        proof += 1
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
@@ -39,8 +43,8 @@ def valid_proof(last_hash, proof):
     IE:  last_hash: ...AE9123456, new hash 123456E88...
     """
 
-    # TODO: Your code here!
-    pass
+    guess = hashlib.sha256(str(proof).encode()).hexdigest()
+    return guess[:6] == last_hash[-6:]
 
 
 if __name__ == '__main__':
